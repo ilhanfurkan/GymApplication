0b85f136cc996ae6a6c54ed59e658305c23c395a
Aydinmutlu7 <76654997+Aydinmutlu7@users.noreply.github.com>
2023-01-03 17:02:17 +0300

    Updates HourValidator,HourController and Hour.

--- Business/Validations/HourValidator.cs
+++ Business/Validations/HourValidator.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Entities.Concrete;
+using FluentValidation;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,21 @@ using System.Threading.Tasks;
 
 namespace Business.Validations
 {
-    public class HourValidator
+    public class HourValidator:AbstractValidator<Hour>
     {
+        public HourValidator() 
+        {
+            RuleFor(hour => hour.Hours).NotEmpty().WithMessage("Boş geçilemez");
+            RuleFor(hour => hour.Hours).MaximumLength(15).WithMessage("en fazla 15 karakter girilir.");
+
+            RuleFor(hour => hour.Time).NotEmpty().WithMessage("Boş geçilemez");
+
+            
+
+
+
+
+
+        }
     }
 }
--- GymGymACoreApplication/Controllers/HourController.cs
+++ GymGymACoreApplication/Controllers/HourController.cs
@@ -1,12 +1,82 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Business.Concrete;
+using Business.Validations;
+using DataAccess.Concrete.EntityFramework;
+using Entities;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Mvc;
 
 namespace GymGymACoreApplication.Controllers
 {
     public class HourController : Controller
     {
+
+        HourManager hm = new HourManager(new EfHourRepository());
         public IActionResult Index()
         {
+            var hour = hm.HourList();
+            return View(hour);
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Add(Hour hour)
+        {
+            HourValidator hourValidator = new HourValidator();
+            var result = hourValidator.Validate(hour);
+            if (result.IsValid)
+            {
+                hm.HourAdd(hour);
+                return RedirectToAction("Index");
+
+            }
+            else
+            {
+                foreach (var item in result.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+                return View();
+            }
+
+        }
+
+        public IActionResult delete(int id)
+        {
+            Hour hour = hm.HourGetById(id);
+            hour.Deleted = true;
+            hm.HourUpdate(hour);
+            return RedirectToAction("Index");
+        }
+        public IActionResult update(int id)
+        {
+            Hour hour = hm.HourGetById(id);
+
+            return View(hour);
+        }
+        [HttpPost]
+        public IActionResult update(Hour hour)
+        {
+            HourValidator hourValidator = new HourValidator();
+            var result = hourValidator.Validate(hour);
+            if (result.IsValid)
+            {
+                hm.HourUpdate(hour);
+                return RedirectToAction("Index");
+
+            }
+            else
+            {
+                foreach (var item in result.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+                return View(hour);
+            }
         }
     }
 }
