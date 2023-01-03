56a25017edbfb196bea5e6cf902bc8e682b6097d
Aydinmutlu7 <76654997+Aydinmutlu7@users.noreply.github.com>
2023-01-03 15:48:46 +0300

    CategoryTrainer add,ındex,delete and update(for and key hataları var unutma!!)

--- Business/Validations/CategoryTrainerValidator.cs
+++ Business/Validations/CategoryTrainerValidator.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Entities;
+using Entities.Concrete;
+using FluentValidation;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,27 @@ using System.Threading.Tasks;
 
 namespace Business.Validations
 {
-    public class CategoryTrainerValidator
+    public class CategoryTrainerValidator : AbstractValidator<CategoryTrainer>
     {
+        public CategoryTrainerValidator() 
+        {
+            RuleFor(categoryTrainer => categoryTrainer.PacketName).NotEmpty().WithMessage("Boş geçilemez");
+            RuleFor(categoryTrainer => categoryTrainer.PacketName).MaximumLength(50).WithMessage("Maximum 50 characters can be entered.");
+            RuleFor(categoryTrainer => categoryTrainer.PacketName).MinimumLength(2).WithMessage("minimum of 2 characters must be entered.");
+
+            RuleFor(categoryTrainer => categoryTrainer.PacketDetail).NotEmpty().WithMessage("Boş geçilemez");
+            RuleFor(categoryTrainer => categoryTrainer.PacketDetail).MaximumLength(150).WithMessage("Maximum 150 characters can be entered.");
+            RuleFor(categoryTrainer => categoryTrainer.PacketDetail).MinimumLength(2).WithMessage("minimum of 2 characters must be entered.");
+
+            RuleFor(categoryTrainer => categoryTrainer.PacketPrice).NotEmpty().WithMessage("Boş geçilemez");
+            RuleFor(categoryTrainer => categoryTrainer.PacketPrice).MaximumLength(50).WithMessage("Maximum 50 characters can be entered.");
+            RuleFor(categoryTrainer => categoryTrainer.PacketPrice).MinimumLength(2).WithMessage("minimum of 2 characters must be entered.");
+
+            RuleFor(categoryTrainer => categoryTrainer.Right).NotEmpty().WithMessage("Boş geçilemez");
+            
+
+
+
+        }
     }
 }
--- GymGymACoreApplication/Controllers/CategoryTrainerController.cs
+++ GymGymACoreApplication/Controllers/CategoryTrainerController.cs
@@ -1,12 +1,83 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Business.Concrete;
+using Business.Validations;
+using DataAccess.Concrete.EntityFramework;
+using Entities;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
 
 namespace GymGymACoreApplication.Controllers
 {
     public class CategoryTrainerController : Controller
     {
+        CategoryTrainerManager ctm = new CategoryTrainerManager(new EfCategoryTrainerRepository());
+
         public IActionResult Index()
         {
+            var categoryTrainer = ctm.CategoryTrainerList();
+            return View(categoryTrainer);
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Add(CategoryTrainer categoryTrainer)
+        {
+            CategoryTrainerValidator categoryTrainerValidator = new CategoryTrainerValidator();
+            var result = categoryTrainerValidator.Validate(categoryTrainer);
+            if (result.IsValid)
+            {
+                ctm.CategoryTrainerAdd(categoryTrainer);
+                return RedirectToAction("Index");
+
+            }
+            else
+            {
+                foreach (var item in result.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+                return View();
+            }
+
+        }
+
+        public IActionResult delete(int id)
+        {
+            CategoryTrainer categoryTrainer = ctm.CategoryTrainerGetById(id);
+            categoryTrainer.Deleted = true;
+            ctm.CategoryTrainerUpdate(categoryTrainer);
+            return RedirectToAction("Index");
+        }
+        public IActionResult update(int id)
+        {
+            CategoryTrainer categoryTrainer = ctm.CategoryTrainerGetById(id);
+
+            return View(categoryTrainer);
+        }
+        [HttpPost]
+        public IActionResult update(CategoryTrainer categoryTrainer)
+        {
+            CategoryTrainerValidator categoryTrainerValidator = new CategoryTrainerValidator();
+            var result = categoryTrainerValidator.Validate(categoryTrainer);
+            if (result.IsValid)
+            {
+                ctm.CategoryTrainerUpdate(categoryTrainer);
+                return RedirectToAction("Index");
+
+            }
+            else
+            {
+                foreach (var item in result.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+                return View(categoryTrainer);
+            }
         }
     }
 }
