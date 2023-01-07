4a39082ed49ddad2da084beb4f04e9ea840e1707
ILHAN-F <furkan.llhan@hotmail.com>
2023-01-07 20:10:00 +0300

    new migration created.  update-database again

---
+++ DataAccess/Migrations/20230107160813_paymentType.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class paymentType : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<bool>(
+                name: "PaymentType",
+                table: "Registrations",
+                type: "bit",
+                nullable: false,
+                oldClrType: typeof(double),
+                oldType: "float");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<double>(
+                name: "PaymentType",
+                table: "Registrations",
+                type: "float",
+                nullable: false,
+                oldClrType: typeof(bool),
+                oldType: "bit");
+        }
+    }
+}
