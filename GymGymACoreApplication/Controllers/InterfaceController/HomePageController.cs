7b6ee0bf9dd3fbdf6e26cff1b227e0fca34a0a74
Yunus emre Şahin <58950075+syunus181@users.noreply.github.com>
2023-01-16 09:02:24 +0300

    HomePage Tema yüklendi

---
+++ GymGymACoreApplication/Controllers/InterfaceController/HomePageController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GymGymACoreApplication.Controllers.InterfaceController
+{
+    public class HomePageController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
