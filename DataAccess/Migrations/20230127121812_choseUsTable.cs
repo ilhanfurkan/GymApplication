7585cbc8f591248adea2cb8f89ef940296445c36
Furkan İLHAN <furkan.llhan@hotmail.com>
2023-01-27 22:47:33 +0300

    new migration

---
+++ DataAccess/Migrations/20230127121812_choseUsTable.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class choseUsTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ChoseUs",
+                columns: table => new
+                {
+                    ChoseUsId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ReasonTitle = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ReasonDetail = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ChoseUsIcon = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ChoseUs", x => x.ChoseUsId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ChoseUs");
+        }
+    }
+}
