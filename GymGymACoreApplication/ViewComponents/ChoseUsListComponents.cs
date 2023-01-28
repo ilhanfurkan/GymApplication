1385111bab143481fba7b83ba557e29b5dd7b181
Furkan İLHAN <furkan.llhan@hotmail.com>
2023-01-28 17:33:46 +0300

    ChoseUs view componenti yapılıdı

---
+++ GymGymACoreApplication/ViewComponents/ChoseUsListComponents.cs
@@ -0,0 +1,26 @@
+﻿using DataAccess.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Entities.Concrete;
+
+namespace GymGymACoreApplication.ViewComponents
+{
+    public class ChoseUsListComponent : ViewComponent
+    {
+        public readonly Context _context;
+
+        public ChoseUsListComponent()
+        {
+            _context = new Context();
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var reasons = _context.ChoseUs.ToList();
+            return View(reasons);
+        }
+
+    }
+}
