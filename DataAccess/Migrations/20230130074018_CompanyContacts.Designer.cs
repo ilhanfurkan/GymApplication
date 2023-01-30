0862f52d30d659674563fb5cbead23d44f880a81
Aydinmutlu7 <76654997+Aydinmutlu7@users.noreply.github.com>
2023-01-30 12:25:51 +0300

    yapılması gerekenler yapıldı eksikler tamamlandı.

--- DataAccess/Migrations/20230130074018_CompanyContacts.Designer.cs
+++ DataAccess/Migrations/20230130074018_CompanyContacts.Designer.cs
@@ -4,6 +4,7 @@ using DataAccess.Concrete;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DataAccess.Migrations
 {
     [DbContext(typeof(Context))]
-    partial class ContextModelSnapshot : ModelSnapshot
+    [Migration("20230130074018_CompanyContacts")]
+    partial class CompanyContacts
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
